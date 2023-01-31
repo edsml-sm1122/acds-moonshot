466d6cd2f4ffbbec94cee6d2a21d03088405cba6
Dingo Luo <luo1ding111@gmail.com>
2023-01-31 17:18:30 +0000

    option checkbox added

--- user_int.py
+++ user_int.py
@@ -12,11 +12,6 @@ class App(tk.Tk):
         self.master = master
 
         self.import_btn = ttk.Button(text="Import Folder", command=self.import_folder)
-
-
-        # label status
-        self.label_label = tk.Label(self.master, text="Images labelled:")
-        self.label_status = tk.Canvas(self.master, height=14, width=14, bg='red')
         
         # Selection between Mars & Moon
         self.planet_selected = tk.StringVar()
@@ -38,20 +33,18 @@ class App(tk.Tk):
 
         # output settings selection box
         self.output_label = tk.Label(self.master, text="Output settings:")
-        self.output_options = ['Original', 
+        self.checkbox_labels = ['Original', 
                         'CDM bounding boxes', 
                         'CDM and true bounding boxes', 
                         'Crater size-frequency distribution', 
                         'Confusion matrix']
-        self.output_var = tk.StringVar(self.master)
-        self.output_var.set(self.output_options[0])
-        self.output_dropdown = tk.OptionMenu(self.master, self.output_var, *self.output_options)
+        self.option_list = [0, 0, 0, 0, 0]
+        
 
-        self.get_output_btn = tk.Button(self.master, text="Submit", command=self.get_all_settings)
+
+        self.get_output_btn = tk.Button(self.master, text="Submit", command=self.submit_functions)
 
         self.import_btn.pack()
-        self.label_label.pack()
-        self.label_status.pack()
         self.mars_rb.pack()
         self.moon_rb.pack()
         self.image_size_label.pack()
@@ -59,7 +52,14 @@ class App(tk.Tk):
         self.IoU_label.pack()
         self.IoU_entry.pack()
         self.output_label.pack()
-        self.output_dropdown.pack()
+        
+        self.update_option_list = []
+        for i in range(5):
+            self.temp_option = tk.IntVar()
+            self.chk = tk.Checkbutton(self.master, text=self.checkbox_labels[i], variable=self.temp_option)
+            self.chk.pack()
+            self.update_option_list.append(self.temp_option)
+        
         self.get_output_btn.pack()
 
 
@@ -79,17 +79,7 @@ class App(tk.Tk):
             files['name'] = remove_ds_store(files['name'])
             files['path'] = remove_ds_store(files['path'])
             df = pd.DataFrame.from_dict(files)
-
-            if os.path.exists(labels_folder):
-                label_check, files = check_label_folder(images_folder, labels_folder)
-                if label_check != "labels":
-                    self.label_status.config(bg='red')
-                    tk.messagebox.showerror('Error', label_check)
-                else:
-                    self.label_status.config(bg='#10cc52')
-                files = remove_ds_store(files)
-                df["labels"] = files
-
+            
             if os.path.exists(locations_folder):
                 locations_folder, files = check_location_folder(locations_folder)
                 if locations_folder != "locations":
@@ -106,24 +96,63 @@ class App(tk.Tk):
             file_list.pack()
             for name in df['name']:
                 file_list.insert(tk.END, name)
+
+            if os.path.exists(labels_folder):
+                label_check, files = check_label_folder(images_folder, labels_folder)
+                if label_check != "labels":
+                    tk.messagebox.showerror('Error', label_check)
+                else:
+                    label_label = tk.Label(self.master, text="The images are labelled!")
+                    #label_status = tk.Canvas(self.master, height=14, width=14, bg='#10cc52')
+                
+                files = remove_ds_store(files)
+                df["labels"] = files
+            else:
+                label_label = tk.Label(self.master, text="The images are not labelled!")
+                #label_status = tk.Canvas(self.master, height=14, width=14, bg='red')
+                #label_status.config(bg='red')
+            
+            label_label.pack(side='top')
+            #label_status.pack(side='top')
+
             return df
 
+
     def get_all_settings(self):
+        for i in range(5):
+            if self.update_option_list[i].get() == 1:
+                self.option_list[i] = 1
+            else:
+                self.option_list[i] = 0
         settings = {
             'Planet': self.planet_selected.get(),
             'IoU': self.IoU_entry.get(),
             'ImageSize': self.image_size_entry.get(),
-            'output': self.output_var.get(), 
+            'Options': self.option_list
         }
         print(settings)
         return settings
 
+
+    def submit_functions(self):
+
+        self.settings = self.get_all_settings()
+        dir_path = filedialog.askdirectory(initialdir=".", title="Create Output Directory", parent=self.master)
+
+        print("Directory saved as:", dir_path)   
+
+        os.mkdir(dir_path + '/' + 'detections')
+        os.mkdir(dir_path + '/' + 'images')
+        os.mkdir(dir_path + '/' + 'statistics')       
+
+
 def main(): 
     root = tk.Tk()
     root.title("Crater Detection Model (CDM)")
-    root.geometry('500x500')
+    #root.geometry('500x500')
     app = App(root)
     root.mainloop()
+
 
 if __name__ == '__main__':
     main()
