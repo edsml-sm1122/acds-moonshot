e348f233d16bb5e48d1cf586417dd49802beb5b5
OlyaTh <112873340+edsml-ot322@users.noreply.github.com>
2023-02-02 23:11:54 +0000

    final changes

--- data_manager.py
+++ data_manager.py
@@ -3,12 +3,13 @@ from os import listdir
 import shutil
 import csv
 import random
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 
 class DataManager:
-  def __init__(self, root_dir, data_dir='data', data_key={'i':'images','l':'labels'} ,dataset_dir = 'dataset'):
+  def __init__(self, root_dir='', data_dir='data', data_key={'i':'images','l':'labels'} ,dataset_dir = 'dataset'):
     self.root_dir    = root_dir
-    self.model_dir   = os.path.join(self.root_dir, model_dir)
     self.data_dir    = os.path.join(self.root_dir, data_dir)
     self.data_key    = data_key
     self.dataset_dir = os.path.join(self.root_dir, dataset_dir)
@@ -30,6 +31,10 @@ class DataManager:
     if delete: 
       try:
         self.delete_if_exists(self.dataset_dir)
+        try: 
+          os.mkdir(self.dataset_dir) 
+        except OSError as error: 
+          return(error)
       except OSError as error:
         return(error)
 
@@ -58,7 +63,7 @@ class DataManager:
     
   def delete_if_exists(self, path_name):
     if os.path.exists(path_name):
-      if os.isdir(path_name):
+      if os.path.isdir(path_name):
         try: 
           shutil.rmtree(path_name)
         except OSError as error: 
@@ -69,60 +74,81 @@ class DataManager:
         except OSError as error: 
           return(error)
     return 0
-
+  
+  def create_dir(self,path_name):
+    try: 
+      os.mkdir(path_name) 
+    except OSError as error: 
+      return(error)
+    return 0
+    
   def convert_csv_to_txt(self):
 
     # we need to convert csv to txt for lables
     for _ in self.model_dirs:
       __ = os.path.join(self.dataset_dir,self.leaves[1],_)
+      print((f'Converting: {__} csv to txt'))
+        
       for i in os.listdir(__):
         csv_file = os.path.join(__,i)
         txt_file = os.path.join(__,i).replace('csv','txt') 
-        print((f'Converting: {csv_file} to {txt_file}'))
         if os.path.exists(txt_file):
           os.remove(txt_file)
-        with open(txt_file, "w") as my_output_file:
+        with open(txt_file, "w", ) as my_output_file:
             with open(csv_file, "r") as my_input_file:
-                [ my_output_file.write("0 "+" ".join(row)+'\n') for row in csv.reader(my_input_file)] 
+              for row in csv.reader(my_input_file):
+                if (" ".join(row).find('x')==-1):  
+                  my_output_file.write("0 "+" ".join(row)+'\n') 
             my_output_file.close()
         os.remove(csv_file)
+    return 0
 
+  def convert_txt_to_csv(self, basepath, destination):
+    os.listdir(basepath)
+    for lbl in os.listdir(basepath):
+      coords = pd.read_csv(os.path.join(basepath,lbl),header=None,index_col=None, sep = " ")
+      coords.drop(columns=coords.columns[-1],  axis=1,  inplace=True)
+      coords.drop(columns=coords.columns[0],  axis=1,  inplace=True)  
+      _=os.path.join(destination,lbl).replace("txt", "csv")
+      coords.to_csv(_,header=None, index=None, sep = ",")  
+      
+    return 0 
 
-  def split_for_model(self, model_chunks={'train':0.7,'val':0.2,'test':0.1}):
+  def split_for_model(self, model_chunks={'train':0.7,'val':0.7,'test':0.3},data_aug=False):
 
-    if(self.model_dirs.size!=model_chunks.size):
+    if(len(self.model_dirs)!=len(model_chunks)):
        raise Exception(f"Dataset folders don't match requested split {self.model_dirs} vs {model_chunks}")
-    elif(self.data_key.size, self.leaves.size):
+    
+    if(len(self.data_key) != len(self.leaves)):
       raise Exception(f"Dataset leaves don't match in size {self.data_key} vs {self.leaves}")
-    else:
-      #splitting the dataset
-      random.seed(41)
+    #splitting the dataset
+    random.seed(41)
 
-      # Read images and labels - this is vital - we assume data folder has 
-      images = os.listdir(os.path.join(self.data_dir,self.data_key['i'])) 
-      labels = os.listdir(os.path.join(self.data_dir,self.data_key['l']))
-      images.sort()
-      labels.sort()
+    # Read images and labels - this is vital - we assume data folder has 
+    images = os.listdir(os.path.join(self.data_dir,self.data_key['i'])) 
+    labels = os.listdir(os.path.join(self.data_dir,self.data_key['l']))
+    images.sort()
+    labels.sort()
 
-      # Split the dataset into train-valid-test splits 
-      train_images, val_images, train_labels, val_labels = train_test_split(images, labels, train_size = model_chunks['train'], random_state = 1)
-      val_images, test_images, val_labels, test_labels = train_test_split(val_images, val_labels, train_size = model_chunks['val'], random_state = 1)
-
-      for _ in self.model_dirs:
-        for _o, _d in zip(self.data_key, self.leaves):
+    # Split the dataset into train-valid-test splits 
+    train_images, tmp_images, train_labels, tmp_labels = train_test_split(images, labels, train_size = model_chunks['train'], random_state = 1)
+    val_images, test_images, val_labels, test_labels = train_test_split(tmp_images, tmp_labels, train_size = model_chunks['val'], random_state = 1)
+    print(f"{len(train_images)=} {len(val_images)=} {len(test_images)=}")
+    print(f"{len(train_labels)=} {len(val_labels)=} {len(test_labels)=}")
+    
+    for _ in self.model_dirs:
+      for _o, _d in zip(np.array(list(self.data_key.values())), self.leaves):
+        print(f'Copying: {_}_{_o}') 
+        for i in locals()[f"{_}_{_o}"]:  
+          origin = os.path.join(self.data_dir,_o,i)
+          dest = os.path.join(self.dataset_dir,_d,_,i)
           
-          print(globals()[f"{_}_{_o}"])
+          #print((f'Copying: {origin} to {dest}'))   
+          try: 
+            shutil.copyfile(origin, dest)
+          except OSError as error: 
+            return(error)
           
-          for i in globals()[f"{_}_{_o}"]:  
-            origin = os.path.join(self.data_dir,_o,i)
-            dest = os.path.join(self.dataset_dir,_d,_,i)
-            
-            print((f'Copying: {origin} to {dest}'))   
-            try: 
-              shutil.copyfile(origin, dest)
-            except OSError as error: 
-              return(error)
-            
-      return 0
+    return 0
 
 
---
+++ model_utils.py
@@ -0,0 +1,114 @@
+import torch
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from PIL import Image
+import shlex, subprocess
+import runpy
+import os
+from pathlib import Path
+
+
+from data_manager import DataManager
+
+
+class MyModel(DataManager):
+    def __init__(self,model_dir='yolov5',mymodel='yolov5x', hyps='hyps/moon_hyp_params.yaml',yaml='moon.yaml'):
+      self.model_dir = model_dir
+      self.model = mymodel
+      self.cfg = f'{mymodel}.yaml'
+      self.weights = f'{mymodel}.pt'
+      self.hyps = hyps
+      self.yaml = yaml
+
+    def predict(self, best_w, img_size=416, conf=0.4, dir_predictions='' ):
+      
+      self.delete_if_exists(path_name= os.path.join(self.model_dir,'runs','detect','exp'))
+     
+      command = f"python {self.model_dir}/detect.py --weights {best_w} --img {img_size} --conf {conf} --source {dir_predictions} --save-txt --save-conf"
+      print(command)
+      args = shlex.split(command)
+      try: 
+        subprocess.run(args, capture_output=True) 
+      except RuntimeError as error: 
+        return(error)
+      
+      return 0
+
+    def plot_test_image(self,img):
+      #plot example test image with lable
+      
+      _ = Image.open(img)
+      fig, ax = plt.subplots()
+      ax.imshow(_, cmap='gray')
+
+      #load the lable
+      lbl = img.replace("images", "labels").replace("png", "txt")
+      my_coords = np.genfromtxt(lbl, delimiter=' ')
+
+      w,h = _.size
+      if my_coords.ndim == 1:
+        rect = patches.Rectangle((my_coords[1]*w-(my_coords[3]*w/2), my_coords[2]*h-(my_coords[4]*h/2)), my_coords[3]*w, my_coords[4]*h, linewidth=1, edgecolor='r', facecolor='none')
+        ax.add_patch(rect)
+      else:  
+        rect={}
+        for i in range(my_coords.shape[0]):
+            rect[i] = patches.Rectangle((my_coords[i][1]*w-(my_coords[i][3]*w/2), my_coords[i][2]*h-(my_coords[i][4]*h/2)), my_coords[i][3]*w, my_coords[i][4]*h, linewidth=1, edgecolor='r', facecolor='none')
+            ax.add_patch(rect[i])
+      plt.show()
+      return
+
+    def plot_train_metircs(self,which_run='yolo_moons5'):
+      #results 5x model
+      _ = Image.open(os.path.join(which_run, 'results.png', ))
+      fig, ax = plt.subplots(figsize=(15, 15))
+      ax.imshow(_)
+
+      _ = Image.open(os.path.join(which_run, 'confusion_matrix.png', ))
+      fig, ax = plt.subplots(figsize=(15, 15))
+      ax.imshow(_)
+
+    def get_predicted_labels_for_images(self, dir_images, planet, output, img_sizes):
+   
+      if planet == 'mars':
+        destination = os.path.join(output, 'detections')
+        self.delete_if_exists(path_name = destination)
+        self.create_dir(path_name = destination)
+
+        pt = f'weights/{planet}_best.pt'
+        
+        self.predict(best_w=pt, img_size=img_sizes[0], conf=0.4, dir_predictions=dir_images)       
+        basepath = self.get_latest_prediction()
+        
+        self.convert_txt_to_csv(basepath, destination)    
+        
+      elif planet == 'moon':
+          for i,size in enumerate(['small','medium','large']):
+            destination = os.path.join(output,f'{size}_detections')
+            
+            self.delete_if_exists(path_name = destination)
+            self.create_dir(path_name = destination)
+            
+            pt = f'weights/{planet}_{size}_best.pt'
+            
+            self.predict(best_w=pt, img_size=img_sizes[i], conf=0.4, dir_predictions=dir_images)
+            basepath = self.get_latest_prediction()
+            print(destination)
+            self.convert_txt_to_csv(basepath, destination)    
+        
+      return 0
+
+    def get_latest_prediction(self):
+        #combine predictions into 1 csv - get the latest
+        paths = sorted(Path(self.model_dir,f'runs/detect/').iterdir(), key=os.path.getmtime)
+        latest = paths[-1]
+        basepath = os.path.join(latest,'labels')
+        print(basepath)
+        return basepath
+
+    def get_predicted_images(self,which_run='exp10'):
+      return os.path.join(self.model_dir,f'runs/detect/{which_run}/')
+
+    def get_predicted_labels(self,which_run='exp10'):
+      return os.path.join(self.model_dir,f'runs/detect/{which_run}/labels/')
