c1ee17f377254ff0ed0686d37a0978e66c9e7283
Dingo Luo <75083697+acse-dl1122@users.noreply.github.com>
2023-02-03 04:06:00 +0000

    Delete Run Me.py

--- Run Me.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-
-url = "https://drive.google.com/drive/folders/1PBk6QbsA7LISa74J5gRS0PCphb06LsCH?usp=share_link"
-response = requests.get(url)
-open("file.zip", "wb").write(response.content)
-os.system("unzip file.zip")
-os.system("python user_int.py")
