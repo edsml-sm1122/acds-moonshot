22c6861ad1fcd37e1f05f0041be86de652fc95dc
Dingo Luo <75083697+acse-dl1122@users.noreply.github.com>
2023-02-03 10:59:39 +0000

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -56,3 +56,34 @@ def tripleStatic(bbpath, tbpath, threshold=0.5, csv_exist = True):
   #triCsv=pd.DataFrame(columns=triName,data=triOut)
   #triCsv.to_csv(outpath, index=False)
   return TP, FP, FN
+
+
+
+def plot_frequency_distribution(imgpath, bbpath, R, image_scale, outpath, tbpath = 'None'):   
+    df = pd.read_csv(bbpath, header = None)
+    rawimg = Image.open(imgpath)
+    w,h = rawimg.size
+    values = (df[[2, 3]].max(axis=1))*w*(image_scale/1000)
+
+    hist, bins = np.histogram(values, bins=70)
+    bin_centers = (bins[:-1] + bins[1:]) / 2
+
+    plt.xlabel("Crater Size (km)")
+    plt.ylabel("Frequency")
+    plt.title("Size-Frequency Distribution of Craters")
+    label1=plt.plot(bin_centers,hist,'r--',label='Detections')
+
+
+    plt.loglog(bin_centers, hist, 'r^-', markersize=10, markeredgecolor='r')
+    if tbpath != 'None':   
+      df2 = pd.read_csv(tbpath, header = None)
+      values2 = (df2[[2, 3]].max(axis=1))*w*(image_scale/1000)
+      hist2, bins2 = np.histogram(values2, bins=70)
+      bin_centers2 = (bins2[:-1] + bins2[1:]) / 2
+      plt.loglog(bin_centers2, hist2, 'bo-', markersize=7, markeredgecolor='b')
+      label2=plt.plot(bin_centers2,hist2,'b--',label='True labels')
+
+    
+    plt.legend()
+    plt.savefig(outpath)
+
