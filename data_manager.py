5cf3381afc6060e1c7cf08f43b734fcd2cf09613
Alice De Schutter <111351902+edsml-aid22@users.noreply.github.com>
2023-02-03 11:44:31 +0000

    update docstrings

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,5 @@
+"""This module is used for the modelling (see the model_utils module)."""
+
 import os
 from os import listdir
 import shutil
@@ -8,16 +10,20 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 class DataManager:
-  """Main data Handler class
-  """
-  def __init__(self, root_dir='', data_dir='data', data_key={'i':'images','l':'labels'} ,dataset_dir = 'dataset'):
-    """Class constructor
+  """ Main data Handler class """
 
-    Args:
+  def __init__(self, root_dir='', data_dir='data', data_key={'i':'images','l':'labels'} ,dataset_dir = 'dataset'):
+    """
+    Class constructor
+
+    Attributes:
+    -----------
+
         root_dir (str, optional): root location of the module. Defaults to ''.
         data_dir (str, optional): data folder with raw data . Defaults to 'data'.
         data_key (dict, optional): standard setup for yolo. Defaults to {'i':'images','l':'labels'}.
         dataset_dir (str, optional): where to store processed data. Defaults to 'dataset'.
+
     """
     self.root_dir    = root_dir
     self.data_dir    = os.path.join(self.root_dir, data_dir)
@@ -28,21 +34,23 @@ class DataManager:
     self.leaves = None
     self.model_dirs = None
 
-  def construct_dataset_tree(self, delete = True, 
-                                   leaves=['images','labels'], 
-                                   model_dirs = ['train','val', 'test'], 
-                                   predictions=False):
-    """Created folder structure for filling up with data
+  def construct_dataset_tree(self, delete = True, leaves=['images','labels'], model_dirs = ['train','val', 'test'], predictions=False):
+    """
+        Created folder structure for filling up with data
 
-    Args:
+        Parameters
+        ----------
         delete (bool, optional): do we want to delete dataset folder and recreate from scratch. Defaults to True.
         leaves (list, optional): Yolo standard structure. Defaults to ['images','labels'].
         model_dirs (list, optional): training set split folders. Defaults to ['train','val', 'test'].
         predictions (bool, optional): optional folder to store images for inference - we are not using this atm. Defaults to False.
 
-    Returns:
+        Returns
+        -------
         int: 0 on success
-    """
+
+        """
+
     #placeholders for future use
     self.leaves = leaves
     self.model_dirs = model_dirs
@@ -82,20 +90,26 @@ class DataManager:
     return 0
     
   def delete_if_exists(self, path_name):
-    """Deletes file or directory 
+    """
+        Deletes file or directory.
 
-    Args:
+        Parameters
+        ----------
         path_name (str): location
 
-    Returns:
+        Returns
+        -------
         int: 0 on success
-    """
+
+        """
+
     if os.path.exists(path_name):
       if os.path.isdir(path_name):
         try: 
           shutil.rmtree(path_name)
         except OSError as error: 
           return(error)  
+
       else:
         try: 
           os.remove(path_name)
@@ -104,26 +118,38 @@ class DataManager:
     return 0
   
   def create_dir(self,path_name):
-    """creates a new dir
+    """
+        Creates a new dir
 
-    Args:
+        Parameters
+        ----------
         path_name (str): location
 
-    Returns:
+        Returns
+        -------
         int: 0 on success
-    """
+
+        """
     try: 
       os.mkdir(path_name) 
     except OSError as error: 
       return(error)
+
     return 0
     
   def convert_csv_to_txt(self):
-    """used to convert csv to txt while copying the data. Yolo expects txt file with lables and classes specified
-
-    Returns:
-        int: return 0 on success
     """
+        Used to convert csv to txt while copying the data. Yolo expects txt file with lables and classes specified.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        int: 0 on success
+
+        """
 
     # we need to convert csv to txt for lables
     for _ in self.model_dirs:
@@ -133,8 +159,10 @@ class DataManager:
       for i in os.listdir(__):
         csv_file = os.path.join(__,i)
         txt_file = os.path.join(__,i).replace('csv','txt') 
+
         if os.path.exists(txt_file):
           os.remove(txt_file)
+
         with open(txt_file, "w", ) as my_output_file:
             with open(csv_file, "r") as my_input_file:
               for row in csv.reader(my_input_file):
@@ -142,19 +170,26 @@ class DataManager:
                   my_output_file.write("0 "+" ".join(row)+'\n') 
             my_output_file.close()
         os.remove(csv_file)
+
     return 0
 
   def convert_txt_to_csv(self, basepath, destination):
-    """convert txt file back to csv file and strip 
+    """
+        Convert txt file back to csv file and strip.
 
-    Args:
+        Parameters
+        ----------
         basepath (str): location of origin
         destination (str): destination
 
-    Returns:
-        int: return 0 on success
-    """
+        Returns
+        -------
+        int: 0 on success
+
+        """
+
     os.listdir(basepath)
+
     for lbl in os.listdir(basepath):
       coords = pd.read_csv(os.path.join(basepath,lbl),header=None,index_col=None, sep = " ")
       coords.drop(columns=coords.columns[-1],  axis=1,  inplace=True)
@@ -165,19 +200,24 @@ class DataManager:
     return 0 
 
   def split_for_model(self, model_chunks={'train':0.7,'val':0.7,'test':0.3},data_aug=False):
-    """Generate model training validation and test sets
+    """
+        Generate model training validation and test sets
 
-    Args:
+        Parameters
+        ----------
         model_chunks (dict, optional): Proportion of the split. Defaults to {'train':0.7,'val':0.7,'test':0.3}.
         data_aug (bool, optional): if we want to play around with the data - this is a potention extension to the model. Defaults to False.
 
-    Raises:
+        Raises:
+        --------
         Exception: if from structure doesn't match to structure, e.g.: images+lables vs images+lables
         Exception: _description_
 
-    Returns:
+        Returns
+        -------
         _type_: _description_
-    """
+
+        """
 
     if(len(self.model_dirs)!=len(model_chunks)):
        raise Exception(f"Dataset folders don't match requested split {self.model_dirs} vs {model_chunks}")
@@ -213,5 +253,3 @@ class DataManager:
             return(error)
           
     return 0
-
-
